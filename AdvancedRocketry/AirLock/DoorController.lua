f240d7080a2931553b0937537bad87934d2cca98
bpmawhin <bpmawhin@ncsu.edu>
2019-02-13 22:10:39 -0500

    add initial functionality, no monitor display

--- AdvancedRocketry/AirLock/DoorController.lua
+++ AdvancedRocketry/AirLock/DoorController.lua
@@ -1,1 +1,93 @@
--- Empty
+-- Enter the sides that the computer is connected to for each block
+
+-- entity detector side (expects a redstone signal)
+local detector = "bottom"
+-- Door that connects to space side of airlock (redstone signal will output to this side)
+local outerDoor = "left"
+-- Door that connects to oxygenated side of airlock (redstone signal will output to this side)
+local innerDoor = "back"
+
+-- name of monitor that leads into airlock from space
+local outerEntranceMonitorName = "monitor_6"
+-- name of monitor that leads into base from airlock
+local innerEntranceMonitorName = "monitor_8"
+-- name of monitor that leads out to space from airlock
+local outerExitMonitorName = "monitor_9"
+-- name of monitor that leads into airlock from base
+local innerExitMonitorName = "monitor_7"
+
+
+-- wrap the monitor names for easier use later
+local outerEntranceMonitor = peripheral.wrap(outerEntranceMonitorName)
+local innerEntranceMonitor = peripheral.wrap(innerEntranceMonitorName)
+local outerExitMonitor = peripheral.wrap(outerExitMonitorName)
+local innerExitMonitor = peripheral.wrap(innerExitMonitorName)
+
+
+-- make waiting nice
+-- wait code
+
+
+
+-- opens the door passed in
+function openDoor(doorName)
+    redstone.setAnalogOutput(doorName,15)
+end
+
+-- closes the door passed in
+function closeDoor(doorName)
+    redstone.setAnalogOutput(doorName,0)
+end
+
+-- returns true if a player is detected in the airlock
+function playerDetected()
+    return redstone.getAnalogInput(detectorSide) != 0
+end
+
+-- opens doors, needs monitor displays
+function ButtonListener()
+    switch = {
+        [outerEntranceMonitorName] = function ()
+            if playerDetected() then
+                -- Access Denied
+            else
+                closeDoor(innerDoor)
+                wait(3)
+                openDoor(outerDoor)
+                wait(3)
+                closeDoor(outerDoor)
+            end
+        end;
+        [innerEntranceMonitorName] = function ()
+            closeDoor(outerDoor)
+            wait(3)
+            openDoor(innerDoor)
+            wait(3)
+            closeDoor(innerDoor)
+        
+        end;
+        [outerExitMonitorName] = function ()
+            closeDoor(innerDoor)
+            wait(3)
+            openDoor(outerDoor)
+            wait(3)
+            closeDoor(outerDoor)
+        
+        end;
+        [innerExitMonitorName] = function ()
+            closeDoor(outerDoor)
+            wait(3)
+            openDoor(innerDoor)
+            wait(3)
+            closeDoor(innerDoor)
+        
+        end
+    }
+    
+    do
+        _,name,_,_ = os.PullEvent("monitor_touch")
+        switch[name]() 
+    end
+end
+
+ButtonListener()
